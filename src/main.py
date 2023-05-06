080df0376b0685c041c66dd7866b1c80d2284ca2
Selv <selv.post@gmail.com>
2023-05-06 22:18:42 +0700

    Implement wind speed display.

--- src/main.py
+++ src/main.py
@@ -8,6 +8,8 @@ weather_data = requests.get(url).json()
 
 temperature = round(weather_data['main']['temp'])
 temperature_feels = round(weather_data['main']['feels_like'])
+wind_speed = round(weather_data['wind']['speed'])
 
 print('Сейчас в городе', city, str(temperature), '°C')
 print('Ощущается как', str(temperature_feels), '°C')
+print('Скорость ветра', str(wind_speed), 'м/с')
